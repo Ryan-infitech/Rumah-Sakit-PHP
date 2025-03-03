b778eba88ebc9718162f72d4298fe45ffcdb11e8
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-03-03 17:38:53 +0700

    model&controller poli

---
+++ app/Models/jadwalpoliklinik.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class jadwalpoliklinik extends Model
+{
+    protected $table = 'jadwalpoliklinik';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->kode = 'JP.' . str_pad(static::count() + 1, 8, '0', STR_PAD_LEFT);
+        });
+    }
+
+    public function dokter()
+    {
+        return $this->belongsTo(Dokter::class);
+    }
+
+    public function poliklinik()
+    {
+        return $this->belongsTo(Poliklinik::class);
+    }
+}
