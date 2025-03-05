2b4bbf1a429a78936c8291dd72a1f3738164e6a4
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-03-05 10:54:04 +0700

    tambah route register

--- routes/web.php
+++ routes/web.php
@@ -41,8 +41,14 @@ Route::delete('/jadwalpoliklinik/{id}', [JadwalpoliklinikController::class, 'des
 
 // Login routes
 Route::middleware(['guest'])->group(function () {
+    // Login routes
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
+
+    // Register routes
+    Route::get('/register', [LoginController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register', [LoginController::class, 'register']);
 });
 
+// Logout route
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
